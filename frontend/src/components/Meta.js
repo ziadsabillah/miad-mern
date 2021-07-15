3b59a9cae0ca4ff3e5da180c3fb46e397b7e9482
Ziad Sabillah <ziadoox.sabillah@gmail.com>
2021-07-15 16:53:25 +0100

    changed meta data

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -12,9 +12,9 @@ const Meta = ({ title, description, keywords }) => {
 }
 
 Meta.defaultProps = {
-  title: 'Welcome To ProShop',
-  description: 'We sell the best products for cheap',
-  keywords: 'electronics, buy electronics, cheap electroincs',
+  title: 'Welcome to MIAD',
+  description: 'We make your appointments easier to manage',
+  keywords: 'healthcare, clinic, clinique, doctor, appointment, rendez-vous',
 }
 
 export default Meta
